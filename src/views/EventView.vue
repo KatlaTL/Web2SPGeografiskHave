a7ddd875096afc4ef6b2e702708de033b364ce74
AndKrogh <anders.krogh99@gmail.com>
2024-04-22 11:44:52 +0200

    adding footer and header to pages

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,8 +1,12 @@
 <script setup>
+import headerComponent from "@/components/HeaderComponent.vue"
+import footerComponent from "@/components/FooterComponent.vue"
 </script>
 
 <template>
   <main>
+    <headerComponent/>
     <h1>EventView</h1>
+    <footerComponent/>
   </main>
 </template>
