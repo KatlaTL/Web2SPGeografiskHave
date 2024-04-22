1e2870757417f16273fac3f53514df6b6e852ac7
AndKrogh <anders.krogh99@gmail.com>
2024-04-22 09:50:17 +0200

    Making footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,7 +2,38 @@
 </script>
 
 <template>
-  <main>
-    <h1>footer</h1>
-  </main>
+<script setup>
+  import '@/assets/styling/footerStyle.css'
+</script>
+
+<template>
+  <footer>
+    <div class="container">
+      <div class="column">
+        <h3>Column 1</h3>
+
+      </div>
+      <div class="column">
+        <h3>Om os</h3>
+        <a href="">Bestyrelsen</a>
+        <a href="">Medarbejder</a>
+        <a href="">Udviklingsplan</a>
+        <a href="">Årsberetninger</a>
+        <a href="">Samarbejdspartner</a>
+        <a href="">Pressebilleder</a>
+        <a href="">Presseperspektiv</a>
+      </div>
+      <div class="column">
+        <h3>Diverse</h3>
+        <p>Content for column 3 goes here.</p>
+        <h3>sociale medier</h3>
+      </div>
+      <div class="column">
+        <h3>Kontakt </h3>
+        <p>Content for column 4 goes here.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
 </template>
