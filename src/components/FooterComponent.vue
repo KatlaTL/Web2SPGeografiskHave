2f2c8f3bb415f0cb01cf3f25318b82349b6b7164
AndKrogh <anders.krogh99@gmail.com>
2024-04-22 14:05:47 +0200

    adding footer and header to pages

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,11 +5,11 @@
 <template>
   <footer>
     <div class="container">
-      <div class="column">
-        <img src="../images and icons/GH til Webudvikling/Ikoner/gh_logo_vertikal_hvid.svg" alt="Geografisk have logo footer">
+      <div class="column column1">
+        <img src="../images and icons/GH til Webudvikling/Ikoner/gh_logo_vertikal_hvid.svg" alt="Geografisk have logo footer" class="GH_logo">
 
       </div>
-      <div class="column">
+      <div class="column column2">
         <h3>Om os</h3>
         <a href="">Bestyrelsen</a>
         <a href="">Medarbejder</a>
@@ -19,20 +19,26 @@
         <a href="">Pressebilleder</a>
         <a href="">Presseperspektiv</a>
       </div>
-      <div class="column">
+      <div class="column column3">
         <h3>Diverse</h3>
         <a href="">Persondatapolitik</a>
         <a href="">Cookiepolitik</a>
         <a href="">Smiley kontrolrapport</a>
-        <h3>sociale medier</h3>
+        <div class="social_media_div">
+          <h3>sociale medier</h3>
+          <img src="../images and icons/GH til Webudvikling/Ikoner/Facebook.svg" alt="">
+          <img src="../images and icons/GH til Webudvikling/Ikoner/Instagram.svg" alt="">  
+        </div>
       </div>
-      <div class="column">
+      <div class="column column3">
         <h3>Kontakt </h3>
         <p>Geografisk Have</p>
         <p>Christian 4 vej 23</p>
         <p>6000 Kolding</p>
-        <p>79797000</p>
-        <p>info@geografiskhave.dk</p>
+        <div class="info_div">
+          <p>79797000</p>
+          <p>info@geografiskhave.dk</p>
+        </div>
       </div>
     </div>
   </footer>
