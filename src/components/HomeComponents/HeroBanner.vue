41d51b7bce8bd8b7dfa5499bdf363c029e0a572a
Asger <regsa13@gmail.com>
2024-04-22 13:29:29 +0200

    added hero banner

---
+++ src/components/HomeComponents/HeroBanner.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div id="hero-banner">
+        <div id="hero-banner-image">
+            <img src="@/assets/images/hero.jpg" alt="Banner events of markeder" />
+        </div>
+        <div id="hero-banner-title">
+            <h1>Events og Markeder</h1>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import headerComponent from "@/components/HeaderComponent.vue"
 import footerComponent from "@/components/FooterComponent.vue"
+import HeroBanner from "@/components/HomeComponents/HeroBanner.vue";
 </script>
 
 <template>
   <headerComponent/>
   <main>
-    <h1>Hello World</h1>
+    <HeroBanner />
+
   </main>
   <footerComponent/>
 </template>
