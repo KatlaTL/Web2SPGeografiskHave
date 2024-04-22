b03999a4a3e328cdf034207cb10a0745a5d3b18d
AndKrogh <anders.krogh99@gmail.com>
2024-04-22 11:46:01 +0200

    adding footer and header to pages

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,11 +1,15 @@
 <script setup>
+import headerComponent from "@/components/HeaderComponent.vue"
+import footerComponent from "@/components/FooterComponent.vue"
 </script>
 
 <template>
   <main>
+    <headerComponent/>
     <h1>CalenderView</h1>
     <router-link :to="{name: 'EventView'}">
-  <button id="myButton">EventView</button>
-</router-link>
+     <button id="myButton">EventView</button>
+    </router-link>
+    <footerComponent/>
   </main>
 </template>
