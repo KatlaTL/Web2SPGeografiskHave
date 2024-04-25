988adccbb9c77be9aed84fe1c15c5ba72b9db1b8
AndKrogh <anders.krogh99@gmail.com>
2024-04-25 12:46:11 +0200

    font and distancing css correction

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -24,18 +24,18 @@ const Events = [
 
 const Tours =[
   [
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "marts 1 - 31 marts, 2024",titel:"Kamelia dage i Væksthuset", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "22 marts, 2024", titel:"Sommersæsonstart i 2024", text: "I Geografisk Have kan man virkelig få lov at nyde naturen i forårets tegn." },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "25 marts, 2024", titel:"Påskeferie // Pileflet workshop", text: "Lær at flette en lille kurv eller et plantestativ af pil" },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "marts 1 - 31 marts, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "22 marts, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "25 marts, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
 
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "6 april , 2024", titel:"Rundvisning i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "25 - 26 maj, 2024", titel:"Plantemarked 2024", text: "Årets Plantemarked afholdes i Geografisk Have lørdag d. 25. maj kl. 10-17 og søndag d. 26. maj kl. 10-17." },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "23 juni, 2024", titel:"Sankthansaften 2024", text: "Lær at flette en lille kurv eller et plantestativ af pil" },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "6 april , 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "25 - 26 maj, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "23 juni, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
 
 
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "23 juni, 2024", titel:"Fællesspisning til Sankthans", text: "Kom til hyggelig fællesspisning med de andre haveklubmedlemmer til Sankthansaften. " },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "17 august, 2024", titel:"Havevandring og foredrag om træet", text: "Kom på en guidet havevandring og hør foredraget om sjældne træer i Geografisk Have med træeksperten Peter Hoffmann." },
-    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "7 september, 2024", titel:"Grøn Have Festival 2024", text: "Kom til Grøn Have Festival og lær, hvordan du kan gøre din have endnu mere “grøn”." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "23 juni, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "17 august, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
+    { src: "src/images and icons/GH til Webudvikling/Billeder til hjemmesiden/rundvisning1.webp", date: "7 september, 2024", titel:"Rundvisninger i Geografisk Have", text: "Vi åbner væksthuset, så du kan komme i forårsstemning og nyde de mange blomstrende kameliaer." },
   ]
 ];
 
