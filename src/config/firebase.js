0b1a0c2515423aff036bb275d1df3d2a92443674
Asger <regsa13@gmail.com>
2024-04-25 11:04:26 +0200

    added firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbKMDAHxQFfB6rJp0fXgtZQS5qyaWBVfE",
+  authDomain: "web2spgeografiskhave.firebaseapp.com",
+  projectId: "web2spgeografiskhave",
+  storageBucket: "web2spgeografiskhave.appspot.com",
+  messagingSenderId: "588389432367",
+  appId: "1:588389432367:web:5320466aa7ee068d5a5ada"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
