ee40cb4c3ff4e2601770593ccd2eaf9fa09ef958
Asger <regsa13@gmail.com>
2024-05-02 16:29:08 +0200

    made address in footer open google maps and fixed some styling issues in the footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,9 @@
-<script setup>
-  import '@/assets/styling/footerStyle.css'
-</script>
-
 <template>
   <footer>
     <div class="container">
       <div class="column column1">
-        <img src="../images and icons/GH til Webudvikling/Ikoner/gh_logo_vertikal_hvid.svg" alt="Geografisk have logo footer" class="GH_logo">
+        <img src="../images and icons/GH til Webudvikling/Ikoner/gh_logo_vertikal_hvid.svg"
+          alt="Geografisk have logo footer" class="GH_logo">
 
       </div>
       <div class="column column2">
@@ -25,16 +22,20 @@
         <a href="#">Cookiepolitik</a>
         <a href="#">Smiley kontrolrapport</a>
         <div class="social_media_div">
-          <h3>sociale medier</h3>
-          <img src="../images and icons/GH til Webudvikling/Ikoner/Facebook.svg" alt="">
-          <img src="../images and icons/GH til Webudvikling/Ikoner/Instagram.svg" alt="">  
+          <h3>Sociale medier</h3>
+          <img src="../images and icons/GH til Webudvikling/Ikoner/Instagram.svg" alt="Instagram logo">
+          <img src="../images and icons/GH til Webudvikling/Ikoner/Facebook.svg" alt="Facebook logo">
         </div>
       </div>
       <div class="column column3">
         <h3>Kontakt </h3>
-        <p>Geografisk Have</p>
-        <p>Christian 4 vej 23</p>
-        <p>6000 Kolding</p>
+        <a class="footer-address"
+          href="https://www.google.com/maps/place/Geografisk+Have/@55.474385,9.4916467,17.25z/data=!4m6!3m5!1s0x464ca1e73acec135:0xaea30a1ba665f139!8m2!3d55.4749931!4d9.4926187!16s%2Fg%2F120_498v?entry=ttu"
+          target="_blank">
+          <p>Geografisk Have</p>
+          <p>Christian 4 vej 23</p>
+          <p>6000 Kolding</p>
+        </a>
         <div class="info_div">
           <p>79797000</p>
           <p>info@geografiskhave.dk</p>
