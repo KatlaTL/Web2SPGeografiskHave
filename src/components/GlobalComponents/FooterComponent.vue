0d148c975b11642327dd14ad7d98e377e4aad4e1
AndKrogh <anders.krogh99@gmail.com>
2024-06-14 10:41:33 +0200

    making tailwindcss

--- src/components/GlobalComponents/FooterComponent.vue
+++ src/components/GlobalComponents/FooterComponent.vue
@@ -1,49 +1,69 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="column column1">
-        <img src="@/assets/icons/gh_logo_vertikal_hvid.svg"
-          alt="Geografisk have logo footer" class="GH_logo">
-
-      </div>
-      <div class="column column2">
-        <h3>Om os</h3>
-        <a href="#">Bestyrelsen</a>
-        <a href="#">Medarbejder</a>
-        <a href="#">Udviklingsplan</a>
-        <a href="#">Årsberetninger</a>
-        <a href="#">Samarbejdspartner</a>
-        <a href="#">Pressebilleder</a>
-        <a href="#">Presseperspektiv</a>
-      </div>
-      <div class="column column3">
-        <h3>Diverse</h3>
-        <a href="#">Persondatapolitik</a>
-        <a href="#">Cookiepolitik</a>
-        <a href="#">Smiley kontrolrapport</a>
-        <div class="social_media_div">
-          <h3>Sociale medier</h3>
-          <img src="@/assets/icons/Instagram.svg" alt="Instagram logo">
-          <img src="@/assets/icons/Facebook.svg" alt="Facebook logo">
+  <footer class="bg-footerBackgroundColor dark:bg-footerBackgroundColor">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-4">
+      <div
+        class="flex lg:justify-between lg:flex-row lg:items-start sm:flex-col text-whiteColor"
+      >
+        <div class="mb-2 md:mb-0">
+          <img
+            src="@/assets/icons/gh_logo_vertikal_hvid.svg"
+            alt="Geografisk have logo footer"
+            class="h-80 w-80 sm:hidden lg:flex"
+          />
         </div>
-      </div>
-      <div class="column column3">
-        <h3>Kontakt </h3>
-        <a class="footer-address"
-          href="https://www.google.com/maps/place/Geografisk+Have/@55.474385,9.4916467,17.25z/data=!4m6!3m5!1s0x464ca1e73acec135:0xaea30a1ba665f139!8m2!3d55.4749931!4d9.4926187!16s%2Fg%2F120_498v?entry=ttu"
-          target="_blank">
-          <p>Geografisk Have</p>
-          <p>Christian 4 vej 23</p>
-          <p>6000 Kolding</p>
-        </a>
-        <div class="info_div">
-          <p>79797000</p>
-          <p>info@geografiskhave.dk</p>
+        <div
+          class="flex flex-col  lg:justify-start lg:items-start py-4 "
+        >
+          <h3 class="pb-4">Om os</h3>
+          <a href="#">Bestyrelsen</a>
+          <a href="#">Medarbejder</a>
+          <a href="#">Udviklingsplan</a>
+          <a href="#">Årsberetninger</a>
+          <a href="#">Samarbejdspartner</a>
+          <a href="#">Pressebilleder</a>
+          <a href="#">Presseperspektiv</a>
+        </div>
+        <div
+          class="flex flex-col sm:justify-center sm:items-start lg:justify-start lg:items-start py-4"
+        >
+          <h3 class="pb-4">Diverse</h3>
+          <a href="#">Persondatapolitik</a>
+          <a href="#">Cookiepolitik</a>
+          <a href="#">Smiley kontrolrapport</a>
+          <div class="py-4">
+            <h3 class="sm:py-2 lg:py-4">Sociale medier</h3>
+            <div class="flex flex-row ">
+              <img src="@/assets/icons/Instagram.svg" alt="Instagram logo" />
+              <img src="@/assets/icons/Facebook.svg" alt="Facebook logo" />
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex flex-col sm:justify-center sm:items-start lg:justify-start lg:items-start py-4"
+        >
+          <h3 class="pb-4">Kontakt</h3>
+          <a
+            class="footer-address"
+            href="https://www.google.com/maps/place/Geografisk+Have/@55.474385,9.4916467,17.25z/data=!4m6!3m5!1s0x464ca1e73acec135:0xaea30a1ba665f139!8m2!3d55.4749931!4d9.4926187!16s%2Fg%2F120_498v?entry=ttu"
+            target="_blank"
+          >
+            <p>Geografisk Have</p>
+            <p>Christian 4 vej 23</p>
+            <p>6000 Kolding</p>
+          </a>
+          <div class="info_div">
+            <p>79797000</p>
+            <p>info@geografiskhave.dk</p>
+          </div>
         </div>
       </div>
     </div>
     <div class="bambus_footer">
-      <img src="@/assets/icons/bambus_footer.png" alt="" class="bambus_footer_img">
+      <img
+        src="@/assets/icons/bambus_footer.png"
+        alt=""
+        class="sm:hidden lg:flex"
+      />
     </div>
   </footer>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,33 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
+    colors: {
+      'headerColor': '#125F31',
+      'whiteColor': '#ffffff',
+      'blackColor': '#000000',
+      'activeGreen': '#7FA761',
+      'yellowColor': '#FFE174',
+      'orangeColor': '#F4A25A',
+      'errorColor': '#FF0000',
+      'backgroundColor': '#FFF9EB',
+      'hoverGreedColor': '#7FA761',
+      'footerBackgroundColor': '#492A14',
+    },
+    screens: {
+      'sm': '100px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
+    fontFamily:{
+      'headersFont':'"Stag", "Open Sans", sans-serif',
+      'bodyTextFont':'"Open Sans", sans-serif'
+    },
     extend: {},
   },
   plugins: [],
