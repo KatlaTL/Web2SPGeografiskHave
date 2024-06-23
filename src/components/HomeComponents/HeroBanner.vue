ace3168c030b01ca244a08403bffc4484df5d597
AndKrogh <anders.krogh99@gmail.com>
2024-06-23 14:31:05 +0200

    small correction

--- src/components/HomeComponents/HeroBanner.vue
+++ src/components/HomeComponents/HeroBanner.vue
@@ -9,12 +9,12 @@ defineProps({
 <template>
     <section class="flex justify-center items-center">
         <div class=" ">
-            <img :src="imageURL" :alt="imageAltText" class="xsm:hidden sm:block"/>
+            <img :src="imageURL" :alt="imageAltText" class="block"/>
         </div>
         <div class="absolute 
          mdsm:top-[33%] md:top-[39%] lg:top-[48%] xl:top-[41%]
          color text-whiteColor">
-            <h1 class="mdsm:text-[30px] md:text-[40px] lg:text-[50px] font-semibold xsm:hidden sm:flex uppercase">{{ title }}</h1>
+            <h1 class="xsm:text-[15px] sm:text-[25px] mdsm:text-[30px] md:text-[40px] lg:text-[50px] font-semibold flex uppercase">{{ title }}</h1>
         </div>
     </section>
 </template>
